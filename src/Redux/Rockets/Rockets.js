117517bb13312cf1708091eaf6d1271c9be39e15
Omar Ragheb <ahmed.raph29@gmail.com>
2022-04-05 20:40:26 +0200

    Create reducer

---
+++ src/Redux/Rockets/Rockets.js
@@ -0,0 +1,25 @@
+import { FETCH_ROCKETS } from '../../constants';
+
+export const fetchRockets = () => async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  const data = await response.json();
+  dispatch({ type: FETCH_ROCKETS, payload: data });
+};
+
+export const rocketsReducer = (state = [], action) => {
+  const rockets = [];
+  switch (action.type) {
+    case FETCH_ROCKETS:
+      action.payload.forEach((rocket) => {
+        rockets.push({
+          id: rocket.rocket_id,
+          name: rocket.rocket_name,
+          type: rocket.rocket_type,
+          flickr_images: rocket.flickr_images[0],
+        });
+      });
+      return rockets;
+    default:
+      return state;
+  }
+};
