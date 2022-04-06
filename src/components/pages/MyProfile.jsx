b64d0187f14a40325dd92196a6aa90f53f84e9e9
Omar Ragheb <ahmed.raph29@gmail.com>
2022-04-07 00:52:06 +0200

    rendering the booked rockets and missions

--- src/components/pages/MyProfile.jsx
+++ src/components/pages/MyProfile.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import style from './MyProfile.module.css';
 
 function MyProfile() {
   const missions = useSelector((state) => state.missions);
   const rockets = useSelector((state) => state.rockets);
-  const [reservedMissions, setReservedMissions] = useState();
-  const [reservedRockets, setReservedRockets] = useState();
+  const [reservedMissions, setReservedMissions] = useState([]);
+  const [reservedRockets, setReservedRockets] = useState([]);
 
   useEffect(() => {
     const resMissions = missions.filter((mission) => mission.reserved && mission);
@@ -14,9 +15,22 @@ function MyProfile() {
     setReservedRockets(resRockets);
   }, []);
 
-  console.log(reservedMissions, reservedRockets);
-
-  return <div>MyProfile</div>;
+  return (
+    <div className={style.container}>
+      <div>
+        <h1>My Missions</h1>
+        <ul className={style.list}>
+          {reservedMissions.map((mission) => <li key={mission.id}>{mission.name}</li>)}
+        </ul>
+      </div>
+      <div>
+        <h1>My Rockets</h1>
+        <ul className={style.list}>
+          {reservedRockets.map((rocket) => <li key={rocket.id}>{rocket.name}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default MyProfile;
